24e9cb91b6d20556ced38a3a9108eb38448ddaa5
lordyhas <dev.haspro@gmail.com>
2023-08-26 23:49:40 +0200

    version 1.0.0+5

--- app/src/main/java/org/unh/i_vote/VoteListActivity.java
+++ app/src/main/java/org/unh/i_vote/VoteListActivity.java
@@ -1,5 +1,6 @@
 package org.unh.i_vote;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,19 +8,27 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Toast;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.unh.i_vote.data.controller.VoteItemAdapter;
+import org.unh.i_vote.data.model.ItemModel;
 import org.unh.i_vote.databinding.ActivityVoteListBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VoteListActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityVoteListBinding binding;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private VoteItemAdapter voteItemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +39,20 @@ public class VoteListActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_vote_list);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_vote_list);
+        //appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Clicked", Snackbar.LENGTH_LONG)
                         .setAnchorView(R.id.fab)
                         .setAction("Action", null).show();
             }
         });
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_vote_list);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
--- app/src/main/java/org/unh/i_vote/ui/vote/VoteViewModel.java
+++ app/src/main/java/org/unh/i_vote/ui/vote/VoteViewModel.java
@@ -1,14 +1,14 @@
-package org.unh.i_vote.ui.gallery;
+package org.unh.i_vote.ui.vote;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class VoteViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public VoteViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
