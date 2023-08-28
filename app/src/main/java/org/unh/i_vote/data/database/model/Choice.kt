27af01391c38cbaeba2e31a93079f0d3c45b4764
lordyhas <dev.haspro@gmail.com>
2023-08-28 15:38:43 +0200

    model : version 1.0.2+0

--- app/src/main/java/org/unh/i_vote/data/database/model/Choice.kt
+++ app/src/main/java/org/unh/i_vote/data/database/model/Choice.kt
@@ -1,14 +1,14 @@
 package org.unh.i_vote.data.database.model
 
 
-data class Choice(val choice: String, val numberOfVote:Int=0){
+data class Choice(val choice: String, var numberOfVote: Long=0){
     companion object{
-        fun fromMap(item: Map<String, Int>): Choice {
-            return Choice(item["choice"] as String, item["numberOfVote"] as Int)
+        fun fromMap(item: Map<String, Long>): Choice {
+            return Choice(item["choice"].toString(), item["numberOfVote"] as Long)
         }
 
-        fun getListFromMap(choiceList: List<Map<String, Int>>): List<Choice> {
-            var choices = mutableListOf<Choice>()
+        fun getListFromMap(choiceList: List<Map<String, Long>>): List<Choice> {
+            val choices = mutableListOf<Choice>()
             choiceList.forEach{ map ->
                 choices.add(Choice.fromMap(map))
             }
--- app/src/main/java/org/unh/i_vote/data/database/model/Vote.kt
+++ app/src/main/java/org/unh/i_vote/data/database/model/Vote.kt
@@ -1,30 +1,70 @@
 package org.unh.i_vote.data.database.model
 
 
+import java.util.Date
+
 data class Vote(
-    val id: Int,
-    val authorId: Int,
+    val id: String,
+    var authorId: String,
+    var authorName: String,
     val isPrivateResult: Boolean = false,
+    val isUniqueChoice: Boolean = false,
+    val title: String,
     val subject: String,
-    val choices: List<String>,
+    val choices: List<Choice>,
+    val endDate: Date,
+    //val orgName: String,
+    //val orgId: String,
 ) {
+
+    fun isEmpty(): Boolean{
+        return this == Vote.empty
+    }
+    fun isNotEmpty(): Boolean{
+        return this != Vote.empty
+    }
+
     fun toMap(): Map<String, Any> {
         return mapOf(
             "id" to id,
             "authorId" to authorId,
+            "authorName" to authorName,
             "isPrivateResult" to isPrivateResult,
             "subject" to subject,
             "choices" to choices,
+            "title" to title,
+            //"orgName" to orgName,
+            //"orgId" to orgId,
+            "endDate" to endDate.time,
+            "isUniqueChoice" to isUniqueChoice,
         );
     }
 
     companion object {
+        val empty = Vote(
+            id = "",
+            authorId= "",
+            authorName ="",
+            isPrivateResult = false,
+            isUniqueChoice = false,
+            title = "",
+            subject = "",
+            choices = emptyList(),
+            endDate = Date()
+        )
         fun fromMap(data: Map<String, Any>): Vote = Vote(
-            id = data["id"] as Int,
-            authorId = data["authorId"] as Int,
+            id = data["id"] as String,
+            authorId = data["authorId"] as String,
+            authorName = data["authorName"] as String,
             isPrivateResult = data["isPrivateResult"] as Boolean,
+            title = data["title"] as String,
             subject = data["subject"] as String,
-            choices = data["choices"] as List<String>,
+            choices = Choice.getListFromMap( data["choices"] as List<Map<String, Long>>),
+            endDate = Date(data["endDate"] as Long),
+            isUniqueChoice = data["isUniqueChoice"] as Boolean,
+            //orgId = data["orgId"] as String,
+            //orgName = data["orgName"] as String,
         );
     }
 }
+
