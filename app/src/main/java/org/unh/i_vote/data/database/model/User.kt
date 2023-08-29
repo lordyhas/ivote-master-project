f757ac9d6ac2e3abf36719aacfbc5f88f93bb263
lordyhas <dev.haspro@gmail.com>
2023-08-29 10:28:25 +0200

    model => User updated : version 1.0.2+2

--- app/src/main/java/org/unh/i_vote/data/database/model/User.kt
+++ app/src/main/java/org/unh/i_vote/data/database/model/User.kt
@@ -3,21 +3,35 @@ package org.unh.i_vote.data.database.model
 data class User(
     val name: String,
     val email: String,
-    val orgList: List<String>
+    val password: String,
+    val orgList: List<String>,
+    val votedList: List<String>
 ){
+    fun isEmpty(): Boolean{
+        return this == User.empty
+    }
+    fun isNotEmpty(): Boolean{
+        return this != User.empty
+    }
     fun toMap() : Map<String, Any> {
         return mapOf(
             "name" to name,
             "email" to email,
+            "password" to password,
             "orgList" to orgList,
+            "votedList" to votedList,
         )
     }
     //fun toJson() = toMap();
     companion object {
+        val empty = User("","","", emptyList(), emptyList())
+
         fun fromMap(data: Map<String, Any>) : User = User(
             data["name"] as String,
             data["email"] as String,
+            data["password"] as String,
             data["orgList"] as List<String>,
+            data["votedList"] as List<String>,
         )
     }
 }
